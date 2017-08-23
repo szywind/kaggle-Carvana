20f3ce1fcf5d2bae11b2f9bacc328774585d8ea6
Zhenyuan Shen <szywind@163.com>
2017-08-23 14:16:28 +0800

    modified unet1024

--- code/unet.py
+++ code/unet.py
@@ -3,7 +3,6 @@ from keras.layers import Input, concatenate, Conv2D, MaxPooling2D, BatchNormaliz
 from keras.optimizers import SGD
 import keras.backend as K
 
-
 def dice_loss(y_true, y_pred):
     smooth = 1.
     y_true_f = K.flatten(y_true)
@@ -409,171 +408,90 @@ def get_unet_512(input_shape=(512, 512, 3),
 
 
 
+
+def block(in_layer, nchan):
+    b1 = Conv2D(nchan, (3, 3), padding='same', kernel_initializer='he_uniform')(in_layer)
+    b1 = BatchNormalization()(b1)
+    b1 = Activation('relu')(b1)
+    b2 = Conv2D(nchan, (3, 3), padding='same')(b1)
+    b2 = BatchNormalization()(b2)
+    b2 = Activation('relu')(b2)
+    b3 = Conv2D(nchan, (3, 3), padding='same')(b2)
+    b3 = BatchNormalization()(b3)
+    b3 = Activation('relu')(b3)
+    c
+    out_layer = Conv2D(nchan, (1, 1), padding='same')(out_layer)
+    return out_layer
+
 def get_unet_1024(input_shape=(1024, 1024, 3),
                   num_classes=1):
     inputs = Input(shape=input_shape)
     # 1024
-
-    down0b = Conv2D(8, (3, 3), padding='same')(inputs)
-    down0b = BatchNormalization()(down0b)
-    down0b = Activation('relu')(down0b)
-    down0b = Conv2D(8, (3, 3), padding='same')(down0b)
-    down0b = BatchNormalization()(down0b)
-    down0b = Activation('relu')(down0b)
+    down0b = block(inputs, 8)
     down0b_pool = MaxPooling2D((2, 2), strides=(2, 2))(down0b)
+
     # 512
-
-    down0a = Conv2D(16, (3, 3), padding='same')(down0b_pool)
-    down0a = BatchNormalization()(down0a)
-    down0a = Activation('relu')(down0a)
-    down0a = Conv2D(16, (3, 3), padding='same')(down0a)
-    down0a = BatchNormalization()(down0a)
-    down0a = Activation('relu')(down0a)
+    down0a = block(down0b_pool, 16)
     down0a_pool = MaxPooling2D((2, 2), strides=(2, 2))(down0a)
+
     # 256
-
-    down0 = Conv2D(32, (3, 3), padding='same')(down0a_pool)
-    down0 = BatchNormalization()(down0)
-    down0 = Activation('relu')(down0)
-    down0 = Conv2D(32, (3, 3), padding='same')(down0)
-    down0 = BatchNormalization()(down0)
-    down0 = Activation('relu')(down0)
+    down0 = block(down0a_pool, 32)
     down0_pool = MaxPooling2D((2, 2), strides=(2, 2))(down0)
+
     # 128
-
-    down1 = Conv2D(64, (3, 3), padding='same')(down0_pool)
-    down1 = BatchNormalization()(down1)
-    down1 = Activation('relu')(down1)
-    down1 = Conv2D(64, (3, 3), padding='same')(down1)
-    down1 = BatchNormalization()(down1)
-    down1 = Activation('relu')(down1)
+    down1 = block(down0_pool, 64)
     down1_pool = MaxPooling2D((2, 2), strides=(2, 2))(down1)
+
     # 64
-
-    down2 = Conv2D(128, (3, 3), padding='same')(down1_pool)
-    down2 = BatchNormalization()(down2)
-    down2 = Activation('relu')(down2)
-    down2 = Conv2D(128, (3, 3), padding='same')(down2)
-    down2 = BatchNormalization()(down2)
-    down2 = Activation('relu')(down2)
+    down2 = block(down1_pool, 128)
     down2_pool = MaxPooling2D((2, 2), strides=(2, 2))(down2)
+
     # 32
-
-    down3 = Conv2D(256, (3, 3), padding='same')(down2_pool)
-    down3 = BatchNormalization()(down3)
-    down3 = Activation('relu')(down3)
-    down3 = Conv2D(256, (3, 3), padding='same')(down3)
-    down3 = BatchNormalization()(down3)
-    down3 = Activation('relu')(down3)
+    down3 = block(down2_pool, 256)
     down3_pool = MaxPooling2D((2, 2), strides=(2, 2))(down3)
+
     # 16
-
-    down4 = Conv2D(512, (3, 3), padding='same')(down3_pool)
-    down4 = BatchNormalization()(down4)
-    down4 = Activation('relu')(down4)
-    down4 = Conv2D(512, (3, 3), padding='same')(down4)
-    down4 = BatchNormalization()(down4)
-    down4 = Activation('relu')(down4)
+    down4 = block(down3_pool, 512)
     down4_pool = MaxPooling2D((2, 2), strides=(2, 2))(down4)
+
     # 8
+    center = block(down4_pool, 1024)
 
-    center = Conv2D(1024, (3, 3), padding='same')(down4_pool)
-    center = BatchNormalization()(center)
-    center = Activation('relu')(center)
-    center = Conv2D(1024, (3, 3), padding='same')(center)
-    center = BatchNormalization()(center)
-    center = Activation('relu')(center)
     # center
-
     up4 = UpSampling2D((2, 2))(center)
     up4 = concatenate([down4, up4], axis=3)
-    up4 = Conv2D(512, (3, 3), padding='same')(up4)
-    up4 = BatchNormalization()(up4)
-    up4 = Activation('relu')(up4)
-    up4 = Conv2D(512, (3, 3), padding='same')(up4)
-    up4 = BatchNormalization()(up4)
-    up4 = Activation('relu')(up4)
-    up4 = Conv2D(512, (3, 3), padding='same')(up4)
-    up4 = BatchNormalization()(up4)
-    up4 = Activation('relu')(up4)
-    # 16
+    up4 = block(up4, 512)
 
+    # 16
     up3 = UpSampling2D((2, 2))(up4)
     up3 = concatenate([down3, up3], axis=3)
-    up3 = Conv2D(256, (3, 3), padding='same')(up3)
-    up3 = BatchNormalization()(up3)
-    up3 = Activation('relu')(up3)
-    up3 = Conv2D(256, (3, 3), padding='same')(up3)
-    up3 = BatchNormalization()(up3)
-    up3 = Activation('relu')(up3)
-    up3 = Conv2D(256, (3, 3), padding='same')(up3)
-    up3 = BatchNormalization()(up3)
-    up3 = Activation('relu')(up3)
-    # 32
+    up3 = block(up3, 256)
 
+    # 32
     up2 = UpSampling2D((2, 2))(up3)
     up2 = concatenate([down2, up2], axis=3)
-    up2 = Conv2D(128, (3, 3), padding='same')(up2)
-    up2 = BatchNormalization()(up2)
-    up2 = Activation('relu')(up2)
-    up2 = Conv2D(128, (3, 3), padding='same')(up2)
-    up2 = BatchNormalization()(up2)
-    up2 = Activation('relu')(up2)
-    up2 = Conv2D(128, (3, 3), padding='same')(up2)
-    up2 = BatchNormalization()(up2)
-    up2 = Activation('relu')(up2)
-    # 64
+    up2 = block(up2, 128)
 
+    # 64
     up1 = UpSampling2D((2, 2))(up2)
     up1 = concatenate([down1, up1], axis=3)
-    up1 = Conv2D(64, (3, 3), padding='same')(up1)
-    up1 = BatchNormalization()(up1)
-    up1 = Activation('relu')(up1)
-    up1 = Conv2D(64, (3, 3), padding='same')(up1)
-    up1 = BatchNormalization()(up1)
-    up1 = Activation('relu')(up1)
-    up1 = Conv2D(64, (3, 3), padding='same')(up1)
-    up1 = BatchNormalization()(up1)
-    up1 = Activation('relu')(up1)
-    # 128
+    up1 = block(up1, 64)
 
+    # 128
     up0 = UpSampling2D((2, 2))(up1)
     up0 = concatenate([down0, up0], axis=3)
-    up0 = Conv2D(32, (3, 3), padding='same')(up0)
-    up0 = BatchNormalization()(up0)
-    up0 = Activation('relu')(up0)
-    up0 = Conv2D(32, (3, 3), padding='same')(up0)
-    up0 = BatchNormalization()(up0)
-    up0 = Activation('relu')(up0)
-    up0 = Conv2D(32, (3, 3), padding='same')(up0)
-    up0 = BatchNormalization()(up0)
-    up0 = Activation('relu')(up0)
-    # 256
+    up0 = block(up0, 32)
 
+    # 256
     up0a = UpSampling2D((2, 2))(up0)
     up0a = concatenate([down0a, up0a], axis=3)
-    up0a = Conv2D(16, (3, 3), padding='same')(up0a)
-    up0a = BatchNormalization()(up0a)
-    up0a = Activation('relu')(up0a)
-    up0a = Conv2D(16, (3, 3), padding='same')(up0a)
-    up0a = BatchNormalization()(up0a)
-    up0a = Activation('relu')(up0a)
-    up0a = Conv2D(16, (3, 3), padding='same')(up0a)
-    up0a = BatchNormalization()(up0a)
-    up0a = Activation('relu')(up0a)
-    # 512
+    up0a = block(up0a, 16)
 
+    # 512
     up0b = UpSampling2D((2, 2))(up0a)
     up0b = concatenate([down0b, up0b], axis=3)
-    up0b = Conv2D(8, (3, 3), padding='same')(up0b)
-    up0b = BatchNormalization()(up0b)
-    up0b = Activation('relu')(up0b)
-    up0b = Conv2D(8, (3, 3), padding='same')(up0b)
-    up0b = BatchNormalization()(up0b)
-    up0b = Activation('relu')(up0b)
-    up0b = Conv2D(8, (3, 3), padding='same')(up0b)
-    up0b = BatchNormalization()(up0b)
-    up0b = Activation('relu')(up0b)
+    up0b = block(up0b, 8)
+
     # 1024
 
     classify = Conv2D(num_classes, (1, 1), activation='sigmoid')(up0b)
